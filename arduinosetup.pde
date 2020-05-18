036e79318cd1474d7623e13ce98b777182548945
azwyane <tunechibaral@gmail.com>
2020-05-18 14:34:19 +0545

    configured serial port

--- arduinosetup.pde
+++ arduinosetup.pde
@@ -1,11 +1,22 @@
+float tempt,humid,pressur; 
+int temppin = 0;
+int humidpin = 1;
+int pressur = 2;
 void setup() {            //This function gets called when the Arduino starts
   Serial.begin(9600);   //This code sets up the Serial port at 9600 baud rate
 }
  
 void loop() {             //This function loops while the arduino is powered
-  int tempt,humid;                //Create an integer variable
-  tempt=analogRead(0);      //Read the analog port 0 where temperature sensor is connected and store the value in tempt
+                 //Create an integer variable
+  tempt=analogRead(0)*0.48828125;      //Read the analog port 0 where temperature sensor is connected and store the value in tempt
   humid=analogRead(1);     //Read humidity from port 1
+  pressur=analogRead(2);
   Serial.println(tempt);    //Print the temperature value to the serial port  Serial.println(humid);
+  Serial.println(humid);
+  Serial.println(pressur);
   delay(30000);            //Wait 30 second before we do it again
 }
+
+
+
+
--- datasender.py
+++ datasender.py
@@ -1,27 +1,49 @@
 import requests
 import serial
-#import sys
+from collections import deque
+from time import sleep
 
-
+delay=30
 
 global tempt,humid,pressure
 URL="https://script.google.com/macros/s/AKfycby8GpFlvX-EWcuwlbONVZVcq0C1ZW80chRC7UdCZO0XG6Db4hXU/exec?"
-data_to_push=[]
-ser = serial.Serial('/dev/ttyUSB0') #can also be '/dev/ttyACM0' check your arduino serial port address
-
-def get_arduino_data_from_usb():
-    data_to_push.append()
-    pass
+data_to_push=deque([])
+#ser = serial.Serial('/dev/ttyUSB0') #can also be '/dev/ttyACM0' check your arduino serial port address
+#ser = serial.Serial('COM15', 9600, timeout=0)
 
 
 def push_arduino_data_to_sheets():
-    for data in data_to_push:
-        URL_with_data=URL+f'temperature={data[tempt]}&humidity={data[humid]}&pressure={data[pressure]}'
-    res=requests.get(URL_with_data)
-if (res.status_code==200):
-    print("Data pushed successfully")
-else:
-    print("Failed to push data")
+    while data_to_push:
+        data=data_to_push.popleft()
+        URL_with_data=URL+f'temperature={data["tempt"]}&humidity={data["humid"]}&pressure={data["pressure"]}'
+        res=requests.get(URL_with_data)
+        if (res.status_code==200):
+            print("Data pushed successfully")
+        else:
+            print("Failed to push data")
+            
+            
+            
+            
+while 1:
+  try:
+            
+    tempt,humid,pressure = ser.readline()   #edit here as per arguments
+    data_to_push.append({"tempt":tempt,"humid":humid,"pressure":pressure})                   # here also
+    push_arduino_data_to_sheets()
+    sleep(delay)
+  except IOError:
+    print('Got some error')
+  sleep(delay)
+
+
+
+#def get_arduino_data_from_usb():
+#    data_to_push.append()
+#    pass
+
+
+
 
 
 #
@@ -30,8 +52,4 @@ else:
 #loop to pop items in that same list until the list is empty
 #
 
-if __name__=='__main__':
-    p1 = Process(target = get_arduino_data_from_usb)
-    p1.start()
-    p2 = Process(target = push_arduino_data_to_sheets)
-    p2.start()
+
